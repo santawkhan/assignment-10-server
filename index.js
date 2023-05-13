9653cdd09cfa401cbbe7a02be4fe3bf84876f268
shanto <santawkha@gmail.com>
2023-05-13 06:11:14 +0600

    server set

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chefData = require('./data/chef.json')
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+});
+
+app.get('/chefData', (req, res) => {
+    res.send(chefData);
+})
+app.get("/chefData/:id", (req, res) => {
+    const id = req.params.id;
+    const item = chefData?.find(pd => pd.id == id)
+    res.send({ item })
+})
+app.listen(port, () => {
+    console.log(`Chef Server is Running:${port}`)
+})
